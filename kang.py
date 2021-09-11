af201392e6917964c37a0d9925630ccfcca2ad80
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-11 21:51:58 +0530

    ~Psycho_Bots~

---
+++ kang.py
@@ -0,0 +1,10 @@
+AA GAYE AUKAAT PPR 
+
+Madarchod Open Source kya hai Kang marne 🤦lage 
+
+BDSK don't Forget Credits 
+to Psychobots + TG bots 
+
+warna Maa Chod li jayagi aapko. 
+
+Muu me laga Haldi Kanger Gand Mara 🙂Jaidli
